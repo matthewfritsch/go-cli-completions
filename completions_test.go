b3ea7ac8a18b66fff6aa16d7e22f73699c222805
Matthew Fritsch <matthewmfritsch@gmail.com>
2024-07-10 21:55:45 -0700

    Actual first run at completion tests

--- completions_test.go
+++ completions_test.go
@@ -4,23 +4,41 @@ import (
 	"testing"
 )
 
-func TestGitCompletion(t *testing.T) {
-	//     name := "Gladys"
-	//     want := regexp.MustCompile(`\b`+name+`\b`)
-	//     msg, err := Hello("Gladys")
-	//     if !want.MatchString(msg) || err != nil {
-	//         t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
-	//     }
-
-	completions, errtype := GenerateCompletions(CompletionType(GIT_BRANCH_A))
+func CheckErrorType(t *testing.T, completions []string, errtype uint8) {
 	switch errtype {
 	case CMD_EXEC_FAIL:
 		t.Fatal("Command execution failed.")
 	case CMD_PIPE_FAIL:
 		t.Fatal("Command setup failed.")
 	default:
-		t.Logf("SUCCESS! Completions is:\n%s\n", completions)
+		words := completions[0]
+		for idx, completion := range completions {
+			if idx != 0 {
+				words += ", " + completion
+			}
+		}
+		t.Logf("SUCCESS! Completions is:\n%s\n", words)
 	}
+}
+
+func TestGitCompletion(t *testing.T) {
+	completions, errtype := GenerateCompletions(CompletionType(GIT_BRANCH_A), "")
+	CheckErrorType(t, completions, errtype)
+}
+
+func TestGitCompletionWithInput(t *testing.T) {
+	completions, errtype := GenerateCompletions(CompletionType(GIT_BRANCH_A), "ma")
+	CheckErrorType(t, completions, errtype)
+}
+
+func TestLS(t *testing.T) {
+	completions, errtype := GenerateCompletions(CompletionType(LS), "")
+	CheckErrorType(t, completions, errtype)
+}
+
+func TestLS_A(t *testing.T) {
+	completions, errtype := GenerateCompletions(CompletionType(LS_A), "")
+	CheckErrorType(t, completions, errtype)
 }
 
 // TestHelloEmpty calls greetings.Hello with an empty string,
--- lib.go
+++ lib.go
@@ -1,7 +1,7 @@
 package go_cli_completion
 
 import (
-	"io"
+	"bytes"
 	"os/exec"
 	"strings"
 )
@@ -9,7 +9,7 @@ import (
 type CompletionType string
 
 const (
-	GIT_BRANCH_A string = "git branch -a"
+	GIT_BRANCH_A string = "git branch -a | sed 's/^\\*//'"
 	LS           string = "ls"
 	LS_A         string = "ls -a"
 )
@@ -20,23 +20,50 @@ const (
 	CMD_EXEC_FAIL
 )
 
-func GenerateCompletions(ct CompletionType) ([]string, uint8) {
-	command_str_split := strings.Split(string(ct), " ")
-	command := command_str_split[0]
-	args := command_str_split[1:]
-	cmd := exec.Command(command, args...)
-	out, err := cmd.StdoutPipe()
+func GenerateCompletions(ct CompletionType, input string) ([]string, uint8) {
+	cmd := exec.Command("bash", "-c", string(ct))
 
-	if err != nil {
-		return []string{}, CMD_PIPE_FAIL
-	}
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
 	if cmd.Run() != nil {
 		return []string{}, CMD_EXEC_FAIL
 	}
 
-	stdout, err := io.ReadAll(out)
-	return strings.Split(string(stdout), " "), SUCCESS
+	// fmt.Printf("STDOUT: %s\n", stdout.String())
+	// fmt.Printf("STDERR: %s\n", stderr.String())
+
+	cleaned := ParsedCompletions(stdout.String())
+	partials := PartialCompletions(cleaned, input)
+	return partials, SUCCESS
+}
+
+func ParsedCompletions(stdout string) []string {
+	lines := strings.Split(stdout, "\n")
+	var trimmed []string
+	for _, line := range lines {
+		t := strings.TrimSpace(line)
+		if t == "" {
+			continue
+		}
+		trimmed = append(trimmed, t)
+	}
+	return trimmed
+}
+
+func PartialCompletions(cleaned []string, input string) []string {
+	var matches []string
+	for _, str := range cleaned {
+		if len(str) < len(input) {
+			continue
+		}
+		if str[0:len(input)] == input {
+			matches = append(matches, str)
+		}
+	}
+	return matches
 }
 
 /*
